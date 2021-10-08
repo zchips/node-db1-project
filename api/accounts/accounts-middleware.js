3b662825ee155383ce164b23f15ffaefdb9879c9
zchips <81702433+zchips@users.noreply.github.com>
2021-10-08 09:10:31 -0600

    middleware wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,5 +1,19 @@
+const { getById, getByName } = require("./accounts-model")
+
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  console.log(typeof req.body.name, typeof req.body.budget)
+if (typeof req.body.name === 'undefined'|| typeof req.body.budget === "undefined") {
+  res.status(400).json({ message: "name and budget are required" })
+} else if (`${typeof req.body.name}` !== "string") {
+  res.status(400).json({ message: "name of account must be a string" })
+} else if (req.body.name.trim().length < 3 || req.body.name.trim().length > 100) {
+  res.status(400).json({ message: "name of account must be between 3 and 100" })
+}  else if (`${typeof req.body.budget}` !== "number") {
+  res.status(400).json({ message: "budget of account must be a number" })
+} else if (req.body.budget < 0 || req.body.budget > 1000000) {
+  res.status(400).json({ message: "budget of account is too large or too small" })
+} else { next() }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,24 @@
+const db = require("../../data/db-config")
+
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db("accounts")
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  return db("accounts").where({id}).first()
 }
 
 const create = account => {
-  // DO YOUR MAGIC
+  return db("accounts").insert(account).then(id=> getById(id[0]))
 }
 
 const updateById = (id, account) => {
-  // DO YOUR MAGIC
+  return db("accounts").insert({id}).update(account).then(id=> getById(id))
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+  return db("accounts").where({ id }).del();
 }
 
 module.exports = {
